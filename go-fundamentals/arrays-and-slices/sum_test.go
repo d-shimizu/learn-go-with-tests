e73fd3b1dc862db0ba813566361fa28e3ea4c277
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-01-05 22:10:10 +0900

    go-fundamentals/arrays-and-slices: Write the minimal amount of code for the test to run and check the failing test output (#3)

--- go-fundamentals/arrays-and-slices/sum_test.go
+++ go-fundamentals/arrays-and-slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -31,8 +34,9 @@ func TestSumAll(t *testing.T) {
 
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
+	//want := "bob"
 
-	if got != want {
+	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
