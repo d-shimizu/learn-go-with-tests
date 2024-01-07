64b48c53ae6379906f72aa950329593fa3c19b96
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-01-07 16:08:31 +0900

    go-fundamentals/structs-methods-and-interfaces: Write enough code to make it pass

--- go-fundamentals/structs-methods-and-interfaces/shapes.go
+++ go-fundamentals/structs-methods-and-interfaces/shapes.go
@@ -1,5 +1,11 @@
 package main
 
+// 長方形の周囲を計算する関数
 func Perimeter(width float64, height float64) float64 {
-	return 0
+	return 2 * (width + height)
+}
+
+// 長方形の面積を計算する関数
+func Area(width float64, height float64) float64 {
+	return width * height
 }
