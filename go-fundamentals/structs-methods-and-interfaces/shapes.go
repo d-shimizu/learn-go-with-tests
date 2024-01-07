8d6a5084a97019f8481ff7545e80956fd500ed14
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-01-07 17:50:55 +0900

    go-fundamentals/structs-methods-and-interfaces: Refactor

--- go-fundamentals/structs-methods-and-interfaces/shapes.go
+++ go-fundamentals/structs-methods-and-interfaces/shapes.go
@@ -1,11 +1,16 @@
 package main
 
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
 // 長方形の周囲を計算する関数
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 // 長方形の面積を計算する関数
-func Area(width float64, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
