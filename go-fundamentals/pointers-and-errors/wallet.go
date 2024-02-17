7d16f8ec6dfef4f71cf581244e5a560fdd428ad8
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-02-17 22:19:08 +0900

    go-fundamentals/pointers-and-errors: Refactor (#1)

--- go-fundamentals/pointers-and-errors/wallet.go
+++ go-fundamentals/pointers-and-errors/wallet.go
@@ -2,15 +2,20 @@ package main
 
 import "fmt"
 
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
-	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
