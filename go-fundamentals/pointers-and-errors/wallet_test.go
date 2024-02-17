938a05ad1122667ce52fb76280d32e84ff5b1b42
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-02-17 22:42:45 +0900

    go-fundamentals/pointers-and-errors: Write the minimal amount of code for the test to run and check the failing test output (#2)

--- go-fundamentals/pointers-and-errors/wallet_test.go
+++ go-fundamentals/pointers-and-errors/wallet_test.go
@@ -21,7 +21,7 @@ func TestWallet(t *testing.T) {
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
-		wallet := Wallet{balance: Bitcoint(20)}
+		wallet := Wallet{balance: Bitcoin(20)}
 
 		wallet.Withdraw(Bitcoin(10))
 
