8edd49c5ed2ed5f7cc19abd382b0c26765143519
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-03-27 21:46:56 +0900

    go-fundamentals/mocking: Refactor (step 2)

--- go-fundamentals/mocking/countdown.go
+++ go-fundamentals/mocking/countdown.go
@@ -12,10 +12,9 @@ const countdownStart = 3
 
 func Countdown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
-		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
 	}
-	time.Sleep(1 * time.Second)
 	fmt.Fprint(out, finalWord)
 }
 
