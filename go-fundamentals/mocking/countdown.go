3a5c4b4cc5d81e6d7e151bc614f98527dc2680c7
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-03-31 21:50:01 +0900

    go-fundamentals/mocking: Still some problems

--- go-fundamentals/mocking/countdown.go
+++ go-fundamentals/mocking/countdown.go
@@ -32,3 +32,19 @@ func main() {
 	sleeper := &DefaultSleeper{}
 	Countdown(os.Stdout, sleeper)
 }
+
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
