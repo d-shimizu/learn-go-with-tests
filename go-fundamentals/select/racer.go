bf01a68cc90818f031cf8c80c76310df2b535f98
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-04-27 11:02:21 +0900

    go-fundamentals/select: Synchronising processes

--- go-fundamentals/select/racer.go
+++ go-fundamentals/select/racer.go
@@ -6,15 +6,23 @@ import (
 )
 
 func Racer(a, b string) (winner string) {
-
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
+}
 
-	return b
+func ping(url string) chan struct{} {
+	// https://qiita.com/najeira/items/47539ab346fa0c00dc62#tips-%E7%A9%BAstruct%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6
+	// struct{} は空の構造体型で、メモリを消費しない
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
 
 func measureResponseTime(url string) time.Duration {
