1973c5074254935636a5b790b6ef4185de95b33e
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-05-11 20:48:51 +0900

    go-fundamentals/reflection: Refactor (v6)

--- go-fundamentals/reflection/reflection.go
+++ go-fundamentals/reflection/reflection.go
@@ -5,23 +5,18 @@ import "reflect"
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	// If the value is a slice, iterate over each element and call walk on it
-	if val.Kind() == reflect.Slice {
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
+		}
+		// If the value is a slice, iterate over each element and call walk on it
+	case reflect.Slice:
 		for i := 0; i < val.Len(); i++ {
 			walk(val.Index(i).Interface(), fn)
 		}
-		return
-	}
-
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
-		}
+	case reflect.String:
+		fn(val.String())
 	}
 }
 
