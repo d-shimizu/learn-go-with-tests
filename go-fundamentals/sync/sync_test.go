00173c3774612f61f6425c1089ae08935082f045
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-05-18 20:31:41 +0900

    go-fundamentals/sync: Refactor (v1)

--- go-fundamentals/sync/sync_test.go
+++ go-fundamentals/sync/sync_test.go
@@ -1,18 +1,24 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
 		counter := Counter{}
-
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
 
-		if counter.Value() != 3 {
-			t.Errorf("got %d, want %d", counter.Value(), 3)
-		}
-
+		assertCounter(t, counter, 3)
 	})
+
+}
+
+func assertCounter(t *testing.T, got Counter, want int) {
+	t.Helper()
+	if got.Value() != 3 {
+		t.Errorf("got %d, want %d", got.Value(), 3)
+	}
 }
