73f6e44045beac6f01e011788e97c46e162b655c
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-05-19 11:58:16 +0900

    go-fundamentals/sync: Copying mutexes

--- go-fundamentals/sync/sync_test.go
+++ go-fundamentals/sync/sync_test.go
@@ -12,7 +12,7 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 		counter.Inc()
 
-		assertCounter(t, counter, 3)
+		assertCounter(t, &counter, 3)
 	})
 
 	t.Run("it runs safely concurrenctly", func(t *testing.T) {
@@ -23,18 +23,17 @@ func TestCounter(t *testing.T) {
 		wg.Add(wantedCount)
 
 		for i := 0; i < wantedCount; i++ {
-			//go func(w *sync.WaitGroup) {
 			go func() {
 				counter.Inc()
 				wg.Done()
 			}()
 		}
 		wg.Wait()
-		assertCounter(t, counter, wantedCount)
+		assertCounter(t, &counter, wantedCount)
 	})
 }
 
-func assertCounter(t *testing.T, got Counter, want int) {
+func assertCounter(t *testing.T, got *Counter, want int) {
 	t.Helper()
 	if got.Value() != want {
 		t.Errorf("got %d, want %d", got.Value(), 3)
