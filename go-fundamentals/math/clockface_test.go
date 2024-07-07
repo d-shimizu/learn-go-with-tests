789264d35f13dea6ab6809dc0e1b3453a0af8031
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-07-07 15:13:47 +0900

    go-fundamentals/math: Write the test first & Try to run the test (v3)

--- go-fundamentals/math/clockface_test.go
+++ go-fundamentals/math/clockface_test.go
@@ -4,18 +4,23 @@ import (
 	"math"
 	"testing"
 	"time"
-
-	"github.com/d-shimizu/learn-go-with-tests/go-fundamentals/math/clockface"
 )
 
 func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{simpleTime(0, 0, 0), Point{0, -1}},
+	}
 
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
-
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondHandPoint(c.time)
+			if got != c.point {
+				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
+			}
+		})
 	}
 }
 
