0cee95d25f8a56f4c3901931da78ad4b06790cc3
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-07-07 20:30:32 +0900

    go-fundamentals/math: Repeat for new requirements & Try to run the test (v4)

--- go-fundamentals/math/clockface_test.go
+++ go-fundamentals/math/clockface_test.go
@@ -4,7 +4,20 @@ import (
 	"math"
 	"testing"
 	"time"
+
+	"github.com/d-shimizu/learn-go-with-tests/go-fundamentals/math/clockface"
 )
+
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+	want := clockface.Point{X: 150, Y: 150 + 90}
+	got := clockface.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
 
 func TestSecondHandAtMidnight(t *testing.T) {
 	cases := []struct {
