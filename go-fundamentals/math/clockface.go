f0331d7a63f35b45fec4817315f4e01b27a06690
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-07-07 21:37:00 +0900

    go-fundamentals/math: Refactor (v4)

--- go-fundamentals/math/clockface.go
+++ go-fundamentals/math/clockface.go
@@ -10,6 +10,10 @@ type Point struct {
 	Y float64
 }
 
+const secondHandLength = 90
+const clockCenterX = 150
+const clockCenterY = 150
+
 func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / (float64(t.Second()))))
 }
@@ -24,8 +28,8 @@ func secondHandPoint(t time.Time) Point {
 
 func SecondHand(t time.Time) Point {
 	p := secondHandPoint(t)
-	p = Point{p.X * 90, p.Y * 90}   // scale
-	p = Point{p.X, -p.Y}            // flip
-	p = Point{p.X + 150, p.Y + 150} // translate
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
+	p = Point{p.X, -p.Y}                                      // flip
+	p = Point{p.X + clockCenterX, p.Y + clockCenterY}         // translate
 	return p
 }
