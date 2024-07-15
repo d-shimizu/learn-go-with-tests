ba0f13bdcd2e5e1356b6b0aee13147e50b8535bb
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-07-15 22:27:21 +0900

    go-fundamentals/math: Parsing XML - Refactor

--- go-fundamentals/math/clockface_test.go
+++ go-fundamentals/math/clockface_test.go
@@ -84,15 +84,73 @@ func TestSVGWriter(t *testing.T) {
 	b := bytes.Buffer{}
 	SVGWriter(&b, tm)
 
-	svg := Svg{}
+	svg := SVG{}
 	xml.Unmarshal(b.Bytes(), &svg)
 
-	x2 := "150.000"
-	y2 := "60.000"
+	x2 := 150.000
+	y2 := 60.000
 
 	for _, line := range svg.Line {
 		if line.X2 == x2 && line.Y2 == y2 {
 			return
 		}
 	}
+}
+
+func TestSVGWriterAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+	b := bytes.Buffer{}
+
+	SVGWriter(&b, tm)
+
+	svg := SVG{}
+
+	xml.Unmarshal(b.Bytes(), &svg)
+	want := Line{150, 150, 150, 60}
+
+	for _, line := range svg.Line {
+		if line == want {
+			return
+		}
+	}
+
+	t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", want, svg.Line)
+}
+
+func TestSVGWriterSecondHand(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{
+			simpleTime(0, 0, 0),
+			Line{150, 150, 150, 60},
+		},
+		{
+			simpleTime(0, 0, 30),
+			Line{150, 150, 150, 240},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			b := bytes.Buffer{}
+			SVGWriter(&b, c.time)
+
+			svg := SVG{}
+			xml.Unmarshal(b.Bytes(), &svg)
+			if !containsLine(c.line, svg.Line) {
+				t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", c.line, svg.Line)
+			}
+		})
+	}
+}
+
+func containsLine(l Line, ls []Line) bool {
+	for _, line := range ls {
+		if line == l {
+			return true
+		}
+	}
+	return false
 }
