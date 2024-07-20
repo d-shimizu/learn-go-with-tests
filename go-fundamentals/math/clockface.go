5515a5ce99520c72d6ef4874fbe044294ee61fbe
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-07-20 22:23:02 +0900

    go-fundamentals/math: Parsing XML - Write the minimal amount of code for the test to run and check the failing test output (v2)

--- go-fundamentals/math/clockface.go
+++ go-fundamentals/math/clockface.go
@@ -93,3 +93,7 @@ func minutesInRadians(t time.Time) float64 {
 	return (secondsInRadians(t) / 60) +
 		(math.Pi / (30 / float64(t.Minute())))
 }
+
+func minuiteHandPoint(t time.Time) Point {
+	return Point{}
+}
