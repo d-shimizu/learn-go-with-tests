f2de1ac78738f26c8c2c9b559daa08876c9ad08d
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-08-04 22:43:12 +0900

    build-an-application/http-server: Refactor (v2-2)

--- build-an-application/http-server/server.go
+++ build-an-application/http-server/server.go
@@ -6,19 +6,27 @@ import (
 	"strings"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 	fmt.Fprint(w, GetPlayerScore(player))
 }
 
-func GetPlayerScore(name string) string {
+func GetPlayerScore(name string) int {
 	if name == "Pepper" {
-		return "20"
+		return 20
 	}
 
 	if name == "Floyd" {
-		return "10"
+		return 10
 	}
 
-	return ""
+	return 0
 }
