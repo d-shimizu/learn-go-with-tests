4510fffebebbf18e5dddfabc5e676cd883e3118c
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-09-15 14:54:26 +0900

    build-an-application/io: Another problem (v5)

--- build-an-application/io/file_system_store.go
+++ build-an-application/io/file_system_store.go
@@ -6,7 +6,7 @@ import (
 )
 
 type FileSystemPlayerStore struct {
-	database io.ReadWriteSeeker
+	database io.Writer
 	league   League
 }
 
@@ -14,8 +14,9 @@ type FileSystemPlayerStore struct {
 func NewFileSystemPlayerStore(database io.ReadWriteSeeker) *FileSystemPlayerStore {
 	database.Seek(0, 0)
 	league, _ := NewLeague(database)
+
 	return &FileSystemPlayerStore{
-		database: database,
+		database: &tape{database},
 		league:   league,
 	}
 }
@@ -45,6 +46,6 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 		f.league = append(f.league, Player{name, 1})
 	}
 
-	f.database.Seek(0, 0)
+	//f.database.Seek(0, 0)
 	json.NewEncoder(f.database).Encode(f.league)
 }
