69ca2f8e2cd20f43e9d853c8dee74c100180746e
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-10-06 21:41:23 +0900

    build-an-application/command-line: Refactor (v3)

--- build-an-application/command-line/cmd/webserver/main.go
+++ build-an-application/command-line/cmd/webserver/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	poker "learn-go-with-test/build-an-application"
 )
@@ -11,18 +10,13 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
-	// OpenFile is used to open a file for reading, writing, or both.
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
-	}
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
-	// FileSystemPlayerStore is a struct that contains a ReadWriteSeeker
-	store, err := poker.NewFileSystemPlayerStore(db)
 	if err != nil {
-		log.Fatalf("problem creating file system player store, %v ", err)
+		log.Fatal(err)
 	}
-	// NewPlayerServer returns a new PlayerServer with a FileSystemPlayerStore
+	defer close()
+
 	server := poker.NewPlayerServer(store)
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
