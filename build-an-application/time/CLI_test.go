8488cacedc9454f49175f8cdf664b7ebebdfe4f0
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-10-12 22:59:14 +0900

    build-an-application/time: Write the test first (v1)

---
+++ build-an-application/time/CLI_test.go
@@ -0,0 +1,43 @@
+package poker_test
+
+import (
+	poker "learn-go-with-test/build-an-application"
+	"strings"
+	"testing"
+)
+
+func TestCLI(t *testing.T) {
+	t.Run("record Chris win from user input", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &poker.StubPlayerStore{}
+
+		// コンストラクタを使ってCLIを作成
+		cli := poker.NewCLI(playerStore, in)
+		cli.PlayPoker()
+
+		poker.AssertPlayerWin(t, playerStore, "Chris")
+	})
+
+	t.Run("record cleo win from user input", func(t *testing.T) {
+		in := strings.NewReader("Cleo wins\n")
+		playerStore := &poker.StubPlayerStore{}
+
+		cli := poker.NewCLI(playerStore, in)
+		cli.PlayPoker()
+
+		poker.AssertPlayerWin(t, playerStore, "Cleo")
+	})
+
+	t.Run("it schedules printing of blind values", func(t *testing.T) {
+		in := strings.NewReader("Chris wins/n")
+		playerStore := &poker.StubPlayerStore{}
+		blindAlerter := &SpyBlindAlerter{}
+
+		cli := poker.NewCLI(playerStore, in, blindAlerter)
+		cli.PlayPoker()
+
+		if len(blindAlerter.alerts) != 1 {
+			t.Fatalf("expected a blind alert to be scheduled")
+		}
+	})
+}
