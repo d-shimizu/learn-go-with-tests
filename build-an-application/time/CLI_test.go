f894632c8b46db7ced656ac436544e5b9e7deab6
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-10-19 00:51:31 +0900

    Write the test first & Try to run the test - 2 (v2)

--- build-an-application/time/CLI_test.go
+++ build-an-application/time/CLI_test.go
@@ -19,6 +19,28 @@ var dummySpyAlerter = &SpyBlindAlerter{}
 
 func TestCLI(t *testing.T) {
 
+	t.Run("record Chris win from user input", func(t *testing.T) {
+
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &poker.StubPlayerStore{}
+
+		// コンストラクタを使ってCLIを作成
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
+		cli.PlayPoker()
+
+		poker.AssertPlayerWin(t, playerStore, "Chris")
+	})
+
+	t.Run("record cleo win from user input", func(t *testing.T) {
+		in := strings.NewReader("Cleo wins\n")
+		playerStore := &poker.StubPlayerStore{}
+
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
+		cli.PlayPoker()
+
+		poker.AssertPlayerWin(t, playerStore, "Cleo")
+	})
+
 	t.Run("it schedules printing of blind values", func(t *testing.T) {
 		in := strings.NewReader("Chris wins\n")
 		playerStore := &poker.StubPlayerStore{}
@@ -37,7 +59,7 @@ func TestCLI(t *testing.T) {
 			{30 * time.Minute, 400},
 			{40 * time.Minute, 500},
 			{50 * time.Minute, 600},
-			{60 * time.Minute, 700},
+			{60 * time.Minute, 800},
 			{70 * time.Minute, 1000},
 			{80 * time.Minute, 2000},
 			{90 * time.Minute, 4000},
@@ -62,41 +84,6 @@ func TestCLI(t *testing.T) {
 					t.Errorf("got scheduled time of %v, want %v", gotScheduledTime, c.expectedScheduleTime)
 				}
 			})
-		}
-	})
-
-	t.Run("record Chris win from user input", func(t *testing.T) {
-
-		in := strings.NewReader("Chris wins\n")
-		playerStore := &poker.StubPlayerStore{}
-
-		// コンストラクタを使ってCLIを作成
-		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
-		cli.PlayPoker()
-
-		poker.AssertPlayerWin(t, playerStore, "Chris")
-	})
-
-	t.Run("record cleo win from user input", func(t *testing.T) {
-		in := strings.NewReader("Cleo wins\n")
-		playerStore := &poker.StubPlayerStore{}
-
-		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
-		cli.PlayPoker()
-
-		poker.AssertPlayerWin(t, playerStore, "Cleo")
-	})
-
-	t.Run("it schedules printing of blind values", func(t *testing.T) {
-		in := strings.NewReader("Chris wins/n")
-		playerStore := &poker.StubPlayerStore{}
-		blindAlerter := &SpyBlindAlerter{}
-
-		cli := poker.NewCLI(playerStore, in, blindAlerter)
-		cli.PlayPoker()
-
-		if len(blindAlerter.alerts) != 1 {
-			t.Fatalf("expected a blind alert to be scheduled")
 		}
 	})
 }
