68cd0c9b83ebf96c54e0889b0969e340a219d3db
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-11-04 20:08:38 +0900

    build-an-application/time: Write enough code to make it pass (v3)

--- build-an-application/time/CLI.go
+++ build-an-application/time/CLI.go
@@ -2,6 +2,7 @@ package poker
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"strings"
 	"time"
@@ -10,18 +11,23 @@ import (
 type CLI struct {
 	playerStore PlayerStore
 	in          *bufio.Scanner
+	out         io.Writer
 	alerter     BlindAlerter
 }
+
+const PlayerPrompt = "Please enter the number of players: "
 
 func NewCLI(store PlayerStore, in io.Reader, out io.Writer, alerter BlindAlerter) *CLI {
 	return &CLI{
 		playerStore: store,
 		in:          bufio.NewScanner(in),
+		out:         out,
 		alerter:     alerter,
 	}
 }
 
 func (cli *CLI) PlayPoker() {
+	fmt.Fprint(cli.out, PlayerPrompt)
 	cli.scheduleBlindAlerts()
 	userInput := cli.readLine()
 	cli.playerStore.RecordWin(extractWinner(userInput))
